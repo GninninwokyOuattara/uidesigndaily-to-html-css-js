baf945cd4f349a6d7d1c822953c72fbda858adb7
Gninninwoky Ouattara <idrissgouattara@gmail.com>
2020-12-09 11:15:50 +0000

    add transition to Accordion

--- Accordion/index.js
+++ Accordion/index.js
@@ -3,7 +3,6 @@ const buttons = document.querySelectorAll(".accordion__item");
 buttons.forEach((button, idx) => {
     button.addEventListener("click", (e) => {
         const item = document.querySelectorAll(".accordion__item");
-        // console.log(item[idx].children[0].classList);
         const btnStatus = button.classList;
         if (btnStatus.contains("opened")) {
             button.classList.remove("opened");
@@ -11,19 +10,22 @@ buttons.forEach((button, idx) => {
                 "fa-plus-circle",
                 "fa-minus-circle"
             );
+            item[idx].children[1].children[1].style.maxHeight = "0px";
         } else {
             button.classList.add("opened");
             item[idx].children[0].classList.replace(
                 "fa-minus-circle",
                 "fa-plus-circle"
             );
+            item[idx].children[1].children[1].style.maxHeight = "500px";
         }
 
-        if (item[idx].children[1].children[1].style.display != "block") {
-            item[idx].children[1].children[1].style.display = "block";
-        } else {
-            item[idx].children[1].children[1].style.display = "none";
-        }
+        // if (item[idx].children[1].children[1].style.display != "block") {
+        //     item[idx].children[1].children[1].style.display = "block";
+        //     item[idx].children[1].children[1].style.maxHeight = "500px";
+        // } else {
+        //     // item[idx].children[1].children[1].style.display = "none";
+        //     item[idx].children[1].children[1].style.maxHeight = "0px";
+        // }
     });
 });
-console.dir(buttons);
